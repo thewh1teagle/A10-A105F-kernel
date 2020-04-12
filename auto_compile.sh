c671115f9d1a3a4a11e997b313c6efe220f10a37
thewh1teagle <>
2020-04-12 23:39:41 +0300

    edited compile script

--- auto_compile.sh
+++ auto_compile.sh
@@ -5,14 +5,20 @@ sleep 1
 ## install tools
 
 echo "installing tools."
-sudo apt-get install git > /dev/null 2>&!
+sudo apt-get install git > /dev/null 2>&1
 
-git clone https://github.com/mscalindt/aarch64-linux-android-4.9.git --progress
+url="https://github.com/mscalindt/aarch64-linux-android-4.9.git"
+folder="aarch64-linux-android-4.9"
+if ! git clone "${url}" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then
+    echo "Clone failed because the folder ${folder} exists"
+fi
 
 cd Kernel
 export CROSS_COMPILE=../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=10
+make clean && make distclean
 make a10_00_defconfig
 make menuconfig
 make -j8
-
+cp arch/arm64/boot/Image ../
+echo "Compiled successfuly, Image file in folder."
